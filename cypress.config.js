de9275e6683e1fa381d9d980d2f8d3e9a8c5fcde
PraveenKumar <praveenct08@gmail.com>
2022-07-21 19:38:47 +0530

    Added dependencies for Allure

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ const addCucumberPreprocessorPlugin =
   require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
 const createEsbuildPlugin =
   require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   e2e: {
@@ -14,12 +15,21 @@ module.exports = defineConfig({
 
       on("file:preprocessor", bundler);
       await addCucumberPreprocessorPlugin(on, config);
+      allureWriter(on, config);
 
       return config;
     },
     specPattern: "cypress/e2e/tests/features/*.feature",
     baseUrl: "https://www.tutorialspoint.com/selenium/selenium_automation_practice.htm",
     screenshotsFolder: "testResults/screenshots",
-    videosFolder: "testResults/videos"
+    videosFolder: "testResults/videos",
+    // env: {
+    //   qaUrl: "",
+    //   stageUrl: "",
+    //   baseUri: "",
+    //   allure: true,
+    //   allureResultsPath: "testResults/allureJson",
+    //   TAGS: "@demouitest"
+    // }
   }
 });
