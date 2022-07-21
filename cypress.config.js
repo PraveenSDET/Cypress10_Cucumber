2f431a1cd5878aaa9b2f64c4f5d471aaac9cc2c9
PraveenKumar <praveenct08@gmail.com>
2022-07-21 17:16:30 +0530

    First commit

---
+++ cypress.config.js
@@ -0,0 +1,25 @@
+const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const addCucumberPreprocessorPlugin =
+  require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const createEsbuildPlugin =
+  require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+
+module.exports = defineConfig({
+  e2e: {
+    async setupNodeEvents(on, config) {
+      const bundler = createBundler({
+        plugins: [createEsbuildPlugin(config)],
+      });
+
+      on("file:preprocessor", bundler);
+      await addCucumberPreprocessorPlugin(on, config);
+
+      return config;
+    },
+    specPattern: "cypress/e2e/tests/features/*.feature",
+    baseUrl: "https://www.tutorialspoint.com/selenium/selenium_automation_practice.htm",
+    screenshotsFolder: "testResults/screenshots",
+    videosFolder: "testResults/videos"
+  }
+});
---
+++ cypress/e2e/tests/steps/HomePage.js
@@ -0,0 +1,26 @@
+import { HomePageFunctions } from "../../applicationLibrary/functionLibrary/HomePageFunctions"
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor"
+
+const homePageFunc = new HomePageFunctions()
+
+Given('user accesses the automation practice site', ()=> {
+    homePageFunc.accessAutomationPracticeSite()
+})
+
+When('user enters first name {string}', (dataset)=> {
+    homePageFunc.enterFirstName(dataset)
+})
+
+When('user extracts the last name label', ()=> {
+    homePageFunc.extractLastNameLabel()
+})
+
+Then('user asserts the label value', ()=> {
+    cy.get('@lastname').then((lname)=> {
+        expect(lname).to.include("Last name")
+    })
+})
+
+When('user clicks the {string} button', (buttonname) => {
+    homePageFunc.clickSubmit(buttonname)
+})
